702cd3e44d08725f55dc12664fb0b2bc943ca058
wangn <wswangning@gmail.com>
2015-05-03 23:11:32 +0800

    log

--- demo/h5/js/script.js
+++ demo/h5/js/script.js
@@ -1,7 +1,18 @@
 var isOK = false;
 
-$(function () {
+// 字体
+(function (doc, win) {
+    var docEl = doc.documentElement, resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize', recalc = function () {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) return;
+        docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+    };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
 
+$(function () {
     //活动规则
     $('.rule').click(function (e) {
         e.preventDefault();
@@ -9,7 +20,7 @@ $(function () {
         $('.rules').removeClass("hide");
     });
     // 关闭弹层
-    $(".mask").click(function(){
+    $(".mask").click(function () {
         e.preventDefault();
         $('.pop').addClass("hide");
         $(".mask").addClass("hide");
@@ -18,31 +29,33 @@ $(function () {
 
 
     //产品介绍
-    $(".product ul li").click(function(e){
+    $(".product ul li").click(function (e) {
         e.preventDefault();
-        $(this).each(function(){
+        $(this).each(function () {
             mask();
+            //动态修改上边距
             $('.pro').removeClass("hide");
+            $(".pro").css("margin-top",-(($(".pro").height())/2));
         });
 
         //免费领取
-        $(".btn-get").click(function(e){
+        $(".btn-get").click(function (e) {
             e.preventDefault();
             $('.pro').addClass("hide");
             $('.apply').removeClass("hide");
             // 提交用户信息
-            $(".btn-submit").click(function(e){
+            $(".btn-submit").click(function (e) {
                 e.preventDefault();
                 validate();
-                if(isOK){
+                if (isOK) {
                     $('.apply').addClass("hide");
                     $('.success').removeClass("hide");
-                }else{
+                } else {
                     return false;
                 }
 
                 // 完成
-                $(".btn-done").click(function(e){
+                $(".btn-done").click(function (e) {
                     e.preventDefault();
                     $(".mask").addClass("hide");
                     $('.pop').addClass("hide");
@@ -52,17 +65,17 @@ $(function () {
     })
 
     // 验证表单
-    function validate(){
+    function validate() {
         var text = $(".text");
         // 文本框不为空
-        if(text.eq(0).val() != "" && text.eq(1).val() != ""){
+        if (text.eq(0).val() != "" && text.eq(1).val() != "") {
             isOK = true;
         }
     }
 
     //遮罩
-    function mask(){
-        $(".mask").css("height",$(document).height());
+    function mask() {
+        $(".mask").css("height", $(document).height());
         $(".mask").removeClass("hide");
         $("body").addClass("overflow");
     }
